6cf5b9c4f9fe2c3c5d5944568cf8d5285f61ec30
Shaoor Munir <shaoormunir@outlook.com>
2019-09-22 04:26:43 +0500

    final changed

--- handler.py
+++ handler.py
@@ -5,23 +5,29 @@ import boto3
 import os
 from decimal import Decimal
 
-def get_service_list_api_url (api_key):
+
+def get_service_list_api_url(api_key):
     return 'https://cloudbilling.googleapis.com/v1/services?key=' + api_key
 
-def get_service_info_api_url (api_key, service_id):
+
+def get_service_info_api_url(api_key, service_id):
     return 'https://cloudbilling.googleapis.com/v1/services/' + service_id + '/skus?key='+api_key
 
-def upload_json_to_s3 (json_data, service_name):
+
+def upload_json_to_s3(json_data, service_name):
     bucket_name = os.environ['S3_BUCKET_NAME']
     json_file_name = service_name + '-' + str(datetime.date.today())+'.json'
     s3_client = boto3.resource('s3')
     s3Object = s3_client.Bucket(bucket_name).Object(json_file_name)
-    s3Object.put(Body=bytes(json.dumps(json_data).encode('UTF-8')), ServerSideEncryption='AES256')
+    s3Object.put(Body=bytes(json.dumps(json_data).encode('UTF-8')),
+                 ServerSideEncryption='AES256')
+
 
 def put_item_to_dynamodb(table_name, item):
     dynamodb_client = boto3.resource('dynamodb')
     table = dynamodb_client.Table(table_name)
     table.put_item(Item=item)
+
 
 def put_service_item_to_db(service_id, service_name):
     table_name = os.environ['SERVICES_TABLE_NAME']
@@ -31,6 +37,7 @@ def put_service_item_to_db(service_id, service_name):
     service_item_dict['service_name'] = service_name
 
     put_item_to_dynamodb(table_name, service_item_dict)
+
 
 def put_sku_item_to_db(service_id, sku_id, sku_description, effective_time):
     table_name = os.environ['SKUS_TABLE_NAME']
@@ -42,6 +49,7 @@ def put_sku_item_to_db(service_id, sku_id, sku_description, effective_time):
     sku_item_dict['effective_time'] = effective_time
 
     put_item_to_dynamodb(table_name, sku_item_dict)
+
 
 def put_tiered_rate_item_to_db(sku_id, start_usage_amount, units, nanos, currency, formatted_price, updated_on):
     table_name = os.environ['RATES_TABLE_NAME']
@@ -62,14 +70,17 @@ def event_handler(event, context):
     # api will be retrieved from aws lambda system environment variable
     api_key = os.environ['GOOGLE_CLOUD_API_KEY']
 
-    compute_services = ['Compute Engine', 'Kubernetes Engine', 'Cloud Run', 'App Engine', 'Cloud Functions']
-    storage_services = ['Cloud Storage', 'Persistent Disk', 'Cloud Filestore','Data Transfer Services', 'Drive Enterprise']
+    compute_services = ['Compute Engine', 'Kubernetes Engine',
+                        'Cloud Run', 'App Engine', 'Cloud Functions']
+    storage_services = ['Cloud Storage', 'Persistent Disk',
+                        'Cloud Filestore', 'Data Transfer Services', 'Drive Enterprise']
     other_services = []
 
     # first step is to get the list of all Google Cloud services
     response = requests.get(get_service_list_api_url(api_key))
 
-    services_json_data = response.json() if response and response.status_code == 200 else None
+    services_json_data = response.json(
+    ) if response and response.status_code == 200 else None
 
     updated_on = datetime.date.today()
     print(updated_on)
@@ -83,15 +94,16 @@ def event_handler(event, context):
 
             put_service_item_to_db(service_id, service_name)
 
-            response = requests.get(get_service_info_api_url(api_key, service.get('serviceId')))
+            response = requests.get(get_service_info_api_url(
+                api_key, service.get('serviceId')))
 
             service_json_data = response.json()
 
             upload_json_to_s3(service_json_data, service_name)
-            
+
             for sku in service_json_data['skus']:
                 # for the second table, get the sku id, the sku description
-            
+
                 sku_id = sku.get('skuId')
                 sku_description = sku.get('description')
                 print(sku_description)
@@ -99,11 +111,12 @@ def event_handler(event, context):
                 effective_time = pricing_info['effectiveTime']
                 print(effective_time)
 
-                put_sku_item_to_db(service_id, sku_id, sku_description, effective_time)
+                put_sku_item_to_db(service_id, sku_id,
+                                   sku_description, effective_time)
 
                 for tiered_rate in pricing_info['pricingExpression']['tieredRates']:
                     # for the third table, get the price, currency, and it will also store upadtion date
-                    
+
                     start_usage_amount = tiered_rate['startUsageAmount']
                     units = tiered_rate['unitPrice']['units']
                     nanos = tiered_rate['unitPrice']['nanos']
@@ -111,13 +124,16 @@ def event_handler(event, context):
                     print(nanos)
                     currency = tiered_rate['unitPrice'].get('currencyCode')
                     formatted_price = int(units, 10) + nanos/1000000000
-                
-                    print(currency)
-                    print ("formatted price is = {:.10f} ".format(formatted_price))
 
-                    put_tiered_rate_item_to_db(sku_id, start_usage_amount, units, nanos, currency, formatted_price, updated_on)
+                    print(currency)
+                    print("formatted price is = {:.10f} ".format(
+                        formatted_price))
+
+                    put_tiered_rate_item_to_db(
+                        sku_id, start_usage_amount, units, nanos, currency, formatted_price, updated_on)
 
     return {
         "message": "Execution of the function was successful.",
         "event": event
+
     }
